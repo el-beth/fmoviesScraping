aca315e514fe47d2d93a5a7fe409af69bf2e5b23
3ndrias <endriasargaw@gmail.com>
2022-10-27 22:37:34 +0300

    automation detection still persistent

--- fmoviesScrape.py
+++ fmoviesScrape.py
@@ -1,9 +1,14 @@
 from selenium import webdriver
-import sys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys as K
+from sys import argv
+from time import sleep
+import re
 
-# if len(sys.argv) == 1:
-# 	exit()
-# url=sys.argv[1]
+if len(argv) == 1:
+	exit()
+# url=argv[1]
+url="https://fmovies.to/movie/the-danish-girl-28p0/1-full"
 
 # construct a chromeoptions object using the constructor method in webdriver
 options = webdriver.ChromeOptions()
@@ -17,6 +22,10 @@ options.add_experimental_option('useAutomationExtension', False)
 # options.add_extension('/home/endu/Downloads/AdBlock — best ad blocker(4.46.0)2022-04-24.crx')
 # dc = DesiredCapabilities.CHROME
 # dc['goog:loggingPrefs'] = { 'browser':'ALL' }
-driver = webdriver.Chrome(options=options, executable_path=r"chromedriver")
+driver = webdriver.Chrome(options=options, executable_path=r"chromedriver_cdc_removed")
+
+def teardown():
+	driver.close()
+	driver.quit()
 
 driver.get(url)
